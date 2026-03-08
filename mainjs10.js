f1e33c21a6780f0a7e558f1a254b74c12967537f
enganisamohamed <enganisamohamed@gmail.com>
2026-03-08 08:20:40 -0700

    adding exercise 10

---
+++ mainjs10.js
@@ -0,0 +1,8 @@
+let library = [
+    { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+    { title: "1984", author: "George Orwell", year: 1949 },
+    { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 }
+];
+
+console.log(library[0].title); 
+console.log(library[1].author); 
